0efcd37db4f5fdffc68d81774dcf6b1756da8c29
Hysterical-Mafia <153342638+Hysterical-Mafia@users.noreply.github.com>
2025-02-10 05:48:50 -0500

    Update and rename 2-2 to 2-2.py

---
+++ Chapter_2/Ch2_Reinforcement/2-2.py
@@ -0,0 +1,4 @@
+ #2-2. Simple Messages: Assign a message to a variable, and print that message. Then change the value of the variable to a new message, and print the new message.
+
+name = ("Hello, I am reading the Python Crash Course 3rd Edition by Eric Matthes!")
+print(name)
