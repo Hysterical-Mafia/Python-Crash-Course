b0be82a9bd1d78af39420d3b43d2a022a43802e6
Hysterical-Mafia <153342638+Hysterical-Mafia@users.noreply.github.com>
2025-02-10 22:57:52 -0500

    Update 2-5.py

--- Chapter_2/Ch2_Reinforcement/2-5.py
+++ Chapter_2/Ch2_Reinforcement/2-5.py
@@ -1,1 +1,3 @@
 # 2-5. Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+
+print("'I'll walk forever with stories inside me that the people I love the most can never hear.'\n -Michelle Hodkin")
