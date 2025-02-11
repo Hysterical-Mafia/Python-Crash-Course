477ef4b1dfb8347d40338f9cc3182025d3da4ec6
Hysterical-Mafia <153342638+Hysterical-Mafia@users.noreply.github.com>
2025-02-10 22:59:50 -0500

    Update 2-6.py

--- Chapter_2/Ch2_Reinforcement/2-6.py
+++ Chapter_2/Ch2_Reinforcement/2-6.py
@@ -1,1 +1,5 @@
+#  2-6. Famous Quote 2: Repeat Exercise 2-5, but this time, represent the famous person’s name using a variable called famous_person. Then compose your message and represent it with a new variable called message. Print your message.
+famous_person= ("-Michelle Hodkin")
+message = ("'I'll walk forever with stories inside me that the people I love the most can never hear.'")
 
+print(f" {message}\n{famous_person}")
