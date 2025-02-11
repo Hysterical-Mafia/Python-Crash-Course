70e082e17060384f2fbf1092d565188f8273dfd3
Hysterical-Mafia <153342638+Hysterical-Mafia@users.noreply.github.com>
2025-02-10 21:11:42 -0500

    Update 2-3.py

--- Chapter_2/Ch2_Reinforcement/2-3.py
+++ Chapter_2/Ch2_Reinforcement/2-3.py
@@ -1,1 +1,5 @@
+ #2-3. Personal Message: Use a variable to represent a person’s name, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
 
+jason = ("Jason Todd")
+
+print(f"Hello {jason}, would you like to learn some python today?")
