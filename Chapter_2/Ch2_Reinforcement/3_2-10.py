31cae5fe3e991efa25cbcf70a1c869d33eaeb248
Hysterical-Mafia <153342638+Hysterical-Mafia@users.noreply.github.com>
2025-02-10 23:32:18 -0500

    Update 3_2-10.py

--- Chapter_2/Ch2_Reinforcement/3_2-10.py
+++ Chapter_2/Ch2_Reinforcement/3_2-10.py
@@ -1,1 +1,10 @@
+'''
+2-10. Favorite Number: Use a variable to represent your favorite number. Then, 
+using that variable, create a message that reveals your favorite number. Print 
+that message.
+'''
 
+favorite_number = 21
+message = f"My favorite number is {favorite_number}"
+
+print(message)
