d1b006adec0277e29007e53aaf998b1ecf229acb
Hysterical-Mafia <153342638+Hysterical-Mafia@users.noreply.github.com>
2025-03-20 23:29:30 -0400

    Update 0.3-1.py

--- Chapter_3/Ch3_Reinforcement/0.3-1.py
+++ Chapter_3/Ch3_Reinforcement/0.3-1.py
@@ -1,1 +1,11 @@
+'''
+3-1. Names: Store the names of a few of your friends in a list called names. Print 
+each person’s name by accessing each element in the list, one at a time.
+'''
 
+names = ["Mario", "Luigi", "Donkey Kong", "Bowser"]
+
+for index, name in enumerate(names, start=1):
+    print(f"Number {index}: {name}")
+
+    
