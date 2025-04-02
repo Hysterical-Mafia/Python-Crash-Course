3533701c6b34c578f2d86ca8ddd444b70c72ff64
Hysterical-Mafia <153342638+Hysterical-Mafia@users.noreply.github.com>
2025-04-02 19:10:14 -0400

    Update 0.3-3.py

--- Chapter_3/Ch3_Reinforcement/0.3-3.py
+++ Chapter_3/Ch3_Reinforcement/0.3-3.py
@@ -1,1 +1,11 @@
+'''
+3-3. Your Own List: Think of your favorite mode of transportation, such as a 
+motorcycle or a car, and make a list that stores several examples. Use your list 
+to print a series of statements about these items, such as “I would like to own a 
+Honda motorcycle.”
+'''
 
+names = ["Bus", "Train", "Car", "Uber", "Motorcycle", "Ferry", "Plane", "Jet", "Bicycle"]
+
+for i in (names):
+    print(f"I would like to take a {i} to school today")
